4cdeb8eec5d4f1a21c9b1e63ffdde06a394f4f8e
Bryan C Guner <bryan.guner@gmail.com>
2022-08-06 00:57:58 +0000

    main

--- src/main.js
+++ src/main.js
@@ -1,35 +1,17 @@
-const newLocal = !((r => {
+! function(r) {
     function n(t) {
-        if (o[t])
-            return o[t].exports;
-        return r[t].call(o[t] = {
+        if (o[t]) return o[t].exports;
+        var e = o[t] = {
             exports: {},
             id: t,
-            loaded: !1,
-        }.exports, o[t] = {
-            exports: {},
-            id: t,
-            loaded: !1,
-        }, o[t] = {
-            exports: {},
-            id: t,
-            loaded: !1,
-        }.exports, n), (o[t] = {
-            exports: {},
-            id: t,
-            loaded: !1,
-        }.loaded = !0), o[t] = {
-            exports: {},
-            id: t,
-            loaded: !1,
-        }.exports;
+            loaded: !1
+        };
+        return r[t].call(e.exports, e, e.exports, n), e.loaded = !0, e.exports
     }
     var o = {};
-    return (n.m = r), (n.c = o), (n.p = "/dist/"), n(0);
-}))([
-    function() {
-        !(function() {
-            throw new Error('Cannot find module "./main.js"');
-        })();
-    },
-]);
+    return n.m = r, n.c = o, n.p = "/dist/", n(0)
+}([function(r, n, o) {
+    ! function() {
+        throw new Error('Cannot find module "./main.js"')
+    }()
+}]);
